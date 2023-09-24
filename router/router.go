4b14fac597113cba82b0cff9c6cd2d40360905e1
maicodsantos <maicon.dsantos@mercadolivre.com>
2023-09-24 11:47:33 -0300

    feat: update handler structure to separate package

--- router/router.go
+++ router/router.go
@@ -3,12 +3,9 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	router := gin.Default()                    // Inicializa o Router utilizando as configurações Default do Gin
-	router.GET("/ping", func(c *gin.Context) { // Define a rota /ping
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router := gin.Default() // Inicializa o Router utilizando as configurações Default do Gin
+
+	initializeRoutes(router) // Inicializa as rotas
 
 	router.Run(":8080") // Executa o servidor na porta 8080 (default
 }
