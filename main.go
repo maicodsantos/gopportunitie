903fbd62381f6328d007636c89b1d97c43165a27
maicodsantos <maicon.dsantos@mercadolivre.com>
2023-09-27 15:33:40 -0300

    Add opening schema package and custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/maicodsantos/gopportunitie/router"
+import (
+	"github.com/maicodsantos/gopportunitie/config"
+	"github.com/maicodsantos/gopportunitie/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init() // Inicializa o Banco de Dados
+	if err != nil {
+		logger.Errorf("config initialization failed: %v", err)
+		return
+	}
+
 	router.Initialize() // Inicializa o Router
 
 }
